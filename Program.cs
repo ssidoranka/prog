bbd9b8582e2c5ca2bb7550a0b50147af5ed29b89
Siamion Sidoranka <w68304@student.wsiz.edu.pl>
2023-10-19 15:49:27 +0200

    lab2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lab2.construction;
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constructions = new Constructions(25, 33, 45, 66, "Brick");
+            Console.WriteLine(constructions.BuildMaterial);
+
+        }
+    }
+}
---
+++ construction/Constructions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2.construction
+{
+    class Constructions
+    {
+        public Constructions(double height, double width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+        public double Height { get; set;  }
+        public double Width { get; set;  }
+        public int Entrances { get; set;  }
+        public int HumanCapacity { get; set;  }
+        public string BuildMaterial { get; set;  }
+    }
+}
+/*1.Height typu zmiennoprzecinkowego
+2. Width typu zmiennoprzecinkowego
+3. Entrances typu liczba całkowita
+4. HumanCapacity typu liczba całkowita
+5. BuildMaterial typu tekstowego
+*/
